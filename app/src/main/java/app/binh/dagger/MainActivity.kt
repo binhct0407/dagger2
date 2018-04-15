5908cba204c76ce7a58da78ed7e0393e4793680c
binhtran07 <binhtd@hblab.vn>
2018-04-15 09:38:45 +0700

    update

--- app/src/main/java/app/binh/dagger/MainActivity.kt
+++ app/src/main/java/app/binh/dagger/MainActivity.kt
@@ -1,12 +1,23 @@
 package app.binh.dagger
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onActionModeFinished(mode: ActionMode?) {
+        super.onActionModeFinished(mode)
+    }
+
+    override fun onActivityReenter(resultCode: Int, data: Intent?) {
+        super.onActivityReenter(resultCode, data)
     }
 }
